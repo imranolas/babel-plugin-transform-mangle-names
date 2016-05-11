5ef1376a8a8b382bda905213d3c2409a2ee1ba3f
Imran <imran@thoughtmachine.net>
2016-05-11 09:17:39 +0100

    Adds test for shadowed vars

---
+++ spec/fixtures/keep-reference-to-shadowed-vars/actual.js
@@ -0,0 +1,7 @@
+const shadowedVar = 4;
+
+function add(longParam, evenLongerParam, shadowedVar) {
+  return longParam + evenLongerParam + shadowedVar;
+};
+
+add(1, 2, 3);
---
+++ spec/fixtures/keep-reference-to-shadowed-vars/expected.js
@@ -0,0 +1,7 @@
+const a = 4;
+
+function add(b, c, d) {
+  return b + c + d;
+};
+
+add(1, 2, 3);
